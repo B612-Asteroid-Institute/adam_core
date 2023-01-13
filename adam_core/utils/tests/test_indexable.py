1de46ff7faf63428ce10ab369b884839ae484f98
Joachim Moeyens <moeyensj@gmail.com>
2023-01-13 13:04:50 -0800

    Add SliceableDataStructureTester to test_indexable to simplify unit tests

--- adam_core/utils/tests/test_indexable.py
+++ adam_core/utils/tests/test_indexable.py
@@ -1,92 +1,132 @@
 import pytest
 import numpy as np
 from astropy.time import Time
+from abc import ABC, abstractmethod
 
 from ..indexable import Indexable
 
-class TestIndexable(Indexable):
+SLICES = [
+    slice(0, 1, 1),  #  0
+    slice(1, 2, 1),  #  1
+    slice(8, 9, 1),  # -1
+    slice(7, 8, 1),  # -2
+    slice(0, 10, 1),
+    slice(0, 5, 1),
+    slice(5, 10, 1),
+]
 
+
+class TestIndexable(Indexable):
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
         Indexable.__init__(self)
 
-SLICES = [
-    slice(0, 1, 1),   #  0
-    slice(1, 2, 1),   #  1
-    slice(8, 9, 1),   # -1
-    slice(7, 8, 1),   # -2
-    slice(0, 10, 1),
-    slice(0, 5, 1),
-    slice(5, 10, 1),
-]
+
+class SliceableDataStructureTester(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def assert_equal(self, a, b):
+        pass
+
+
+class NumpyArrayTester(SliceableDataStructureTester):
+    def __init__(self):
+        array = np.arange(0, 100)
+        self.values = array
+
+    def assert_equal(self, a, b):
+        if isinstance(a, (np.int_, np.float_)):
+            assert a == b
+        else:
+            np.testing.assert_equal(a, b)
+
+
+class NumpyMaskedArrayTester(SliceableDataStructureTester):
+    def __init__(self):
+        masked_array = np.ma.arange(0, 10)
+        masked_array.mask = np.zeros(len(masked_array))
+        masked_array.mask[0:10:2] = 1
+        self.values = masked_array
+
+    def assert_equal(self, a, b):
+        if isinstance(a, (np.int_, np.float_)):
+            assert a == b
+        else:
+            np.testing.assert_equal(a.data, b.data)
+            np.testing.assert_equal(a.mask, b.mask)
+
+
+class AstropyTimeTester(SliceableDataStructureTester):
+    def __init__(self):
+        times = Time(np.arange(59000, 59100), scale="utc", format="mjd")
+        self.values = times
+
+    def assert_equal(self, a, b):
+        if isinstance(a, (np.int_, np.float_)):
+            assert a == b
+        else:
+            np.testing.assert_equal(a.mjd, b.mjd)
+            assert a.scale == b.scale
+            assert a.format == b.format
+
 
 def test_Indexable_slicing_array():
 
-    array = np.arange(0, 10)
-
-    indexable = TestIndexable(array=array)
+    tester = NumpyArrayTester()
+    indexable = TestIndexable(values=tester.values)
     for s in SLICES:
-        np.testing.assert_equal(indexable[s].array, indexable.array[s])
+        tester.assert_equal(indexable[s].values, indexable.values[s])
 
     return
+
 
 def test_Indexable_slicing_marray():
 
-    masked_array = np.ma.arange(0, 10)
-    masked_array.mask = np.zeros(len(masked_array))
-    masked_array.mask[0:10:2] = 1
-
-    indexable = TestIndexable(masked_array=masked_array)
+    tester = NumpyMaskedArrayTester()
+    indexable = TestIndexable(values=tester.values)
     for s in SLICES:
-        np.testing.assert_equal(indexable[s].masked_array.data, indexable.masked_array[s].data)
-        np.testing.assert_equal(indexable[s].masked_array.mask, indexable.masked_array[s].mask)
+        tester.assert_equal(indexable[s].values, indexable.values[s])
 
     return
 
+
 def test_Indexable_slicing_time():
 
-    times = Time(np.arange(59000, 59010), scale="utc", format="mjd")
-
-    indexable = TestIndexable(times=times)
+    tester = AstropyTimeTester()
+    indexable = TestIndexable(values=tester.values)
     for s in SLICES:
-        np.testing.assert_equal(indexable[s].times.mjd, indexable.times[s].mjd)
-        assert indexable[s].times.scale == indexable.times[s].scale
-        assert indexable[s].times.format == indexable.times[s].format
+        tester.assert_equal(indexable[s].values, indexable.values[s])
 
     return
 
 def test_Indexable_iteration_array():
 
-    array = np.arange(0, 10)
-
-    indexable = TestIndexable(array=array)
+    tester = NumpyArrayTester()
+    indexable = TestIndexable(values=tester.values)
     for i, indexable_i in enumerate(indexable):
-        np.testing.assert_equal(indexable_i.array[0], array[i])
+        tester.assert_equal(indexable_i.values[0], tester.values[i])
 
     return
 
 def test_Indexable_iteration_marray():
 
-    masked_array = np.ma.arange(0, 10)
-    masked_array.mask = np.zeros(len(masked_array))
-    masked_array.mask[0:10:2] = 1
-
-    indexable = TestIndexable(masked_array=masked_array)
-    for i, ind in enumerate(indexable):
-        np.testing.assert_equal(ind.masked_array.data[0], masked_array.data[i])
-        np.testing.assert_equal(ind.masked_array.mask[0], masked_array.mask[i])
+    tester = NumpyMaskedArrayTester()
+    indexable = TestIndexable(values=tester.values)
+    for i, indexable_i in enumerate(indexable):
+        tester.assert_equal(indexable_i.values[0], tester.values[i])
 
     return
 
 def test_Indexable_iteration_time():
 
-    times = Time(np.arange(59000, 59010), scale="utc", format="mjd")
-
-    indexable = TestIndexable(times=times)
-    for i, ind in enumerate(indexable):
-        np.testing.assert_equal(ind.times.mjd[0], times.mjd[i])
+    tester = AstropyTimeTester()
+    indexable = TestIndexable(values=tester.values)
+    for i, indexable_i in enumerate(indexable):
+        tester.assert_equal(indexable_i.values[0], tester.values[i])
 
     return
-
