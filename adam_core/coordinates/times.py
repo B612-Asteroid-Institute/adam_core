d2ad1cfecd15539ceabbe213f3ee9c307278c7bc
Joachim Moeyens <moeyensj@users.noreply.github.com>
2023-05-25 10:34:56 -0700

    Store times as two floats
    Co-authored-by: Spencer Nelson <spencer@b612foundation.org>

--- adam_core/coordinates/times.py
+++ adam_core/coordinates/times.py
@@ -5,17 +5,26 @@ from quivr import Float64Field, StringField, Table
 
 class Times(Table):
 
-    # TODO: @spenczar - We could have this class store times
-    # as two integers... :)
-    mjd = Float64Field(nullable=False)
-
-    # Could replace this with an enum of some kind
+    # Stores the time as a pair of float64 values in the same style as erfa/astropy:
+    # The first one is the day-part of a Julian date, and the second is
+    # the fractional day-part.
+    jd1 = Float64Field(nullable=False)
+    jd2 = Float64Field(nullable=False)
     scale = StringField(nullable=False)
 
     @classmethod
     def from_astropy(cls, time: Time):
         scale = np.array([time.scale for i in range(len(time))])
-        return cls.from_kwargs(mjd=time.mjd, scale=scale)
+        return cls.from_kwargs(jd1=time.jd1, jd2=time.jd2, scale=scale)
 
-    def to_astropy(self) -> Time:
-        return Time(self.mjd.to_numpy(), format="mjd", scale=str(self.scale[0]))
+    def to_astropy(self, format: str = "jd") -> Time:
+        t = Time(
+            val=self.jd1.to_numpy(),
+            val2=self.jd2.to_numpy(),
+            format="jd",
+            scale=self.scale[0].as_py(),
+        )
+        if format == "jd":
+            return t
+        t.format = format
+        return t
