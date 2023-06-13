Bug fix: Remove additional factor of anomaly in the derivative of the hyperbolic Kepler equation

adam_core/dynamics/kepler.py
@@ -136,7 +136,7 @@ def solve_kepler(e: float, M: float, max_iter: int = 100, tol: float = 1e-15) ->
         # Newton-Raphson
         # Equation 3.45 in Curtis (2014) [1]
         f = e * jnp.sinh(F) - F - M
-        fp = e * jnp.cosh(F) * F - 1
+        fp = e * jnp.cosh(F) - 1
 
         ratio = f / fp
         F -= ratio