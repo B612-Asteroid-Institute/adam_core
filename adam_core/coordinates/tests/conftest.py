9364f9fb86233e67f873f1a805fab6934eee5fb0
Joachim Moeyens <moeyensj@gmail.com>
2023-06-13 16:11:49 -0700

    Add unit tests for jax vmapped Keplerian<->Cartesian transformations

---
+++ adam_core/coordinates/tests/conftest.py
@@ -0,0 +1,15 @@
+from importlib.resources import files
+
+import pandas as pd
+import pytest
+
+
+@pytest.fixture
+def orbital_elements():
+    orbital_elements_file = files("adam_core.utils.helpers.data").joinpath(
+        "elements.csv"
+    )
+    df = pd.read_csv(
+        orbital_elements_file, index_col=False, float_precision="round_trip"
+    )
+    return df
