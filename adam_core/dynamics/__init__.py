4a9205997f2f0b60c994ea1c5962779be5aa22b5
Joachim Moeyens <moeyensj@gmail.com>
2023-07-05 09:41:16 -0700

    Add calc_chi

--- adam_core/dynamics/__init__.py
+++ adam_core/dynamics/__init__.py
@@ -1,5 +1,6 @@
 # flake8: noqa: F401
 from .barker import solve_barker
+from .chi import calc_chi
 from .kepler import solve_kepler
 from .stumpff import calc_stumpff
 from .tisserand import calc_tisserand_parameter
