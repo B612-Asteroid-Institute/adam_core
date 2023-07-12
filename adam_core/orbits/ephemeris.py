45f2d3bb51ac44126836a8dedfa8b7532aba73e8
Joachim Moeyens <moeyensj@gmail.com>
2023-07-12 12:38:03 -0700

    Make Observers a subtable of Ephemeris

--- adam_core/orbits/ephemeris.py
+++ adam_core/orbits/ephemeris.py
@@ -1,22 +1,20 @@
 from quivr import Float64Column, StringAttribute, StringColumn, Table
 
-from ..coordinates.origin import Origin
 from ..coordinates.spherical import SphericalCoordinates
-from ..coordinates.times import Times
+from ..observers import Observers
 
 
 class Ephemeris(Table):
 
     orbit_id = StringColumn(nullable=False)
     object_id = StringColumn(nullable=False)
-    time = Times.as_column(nullable=False)
-    rho = Float64Column(nullable=False)
+    observer = Observers.as_column(nullable=True)
+    rho = Float64Column(nullable=True)
     ra = Float64Column(nullable=False)
     dec = Float64Column(nullable=False)
-    vrho = Float64Column(nullable=False)
-    vra = Float64Column(nullable=False)
-    vdec = Float64Column(nullable=False)
-    origin = Origin.as_column(nullable=False)
+    vrho = Float64Column(nullable=True)
+    vra = Float64Column(nullable=True)
+    vdec = Float64Column(nullable=True)
     frame = StringAttribute()
 
     def as_spherical_coordinates(self):
@@ -27,7 +25,7 @@ class Ephemeris(Table):
             vrho=self.vrho,
             vlon=self.vra,
             vlat=self.vdec,
-            time=self.time,
-            origin=self.origin,
+            time=self.observer.coordinates.time,
+            origin=self.observer.code,
             frame=self.frame,
         )
