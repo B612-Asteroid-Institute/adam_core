3c498febef6f0183cd463e70edafbb8269de15b2
Joachim Moeyens <moeyensj@gmail.com>
2023-08-14 05:46:15 -0700

    Add VariantOrbits

---
+++ adam_core/orbits/variants.py
@@ -0,0 +1,62 @@
+import uuid
+from typing import Literal
+
+import pyarrow.compute as pc
+import quivr as qv
+
+from ..coordinates.cartesian import CartesianCoordinates
+from ..coordinates.variants import create_coordinate_variants
+from .orbits import Orbits
+
+
+class VariantOrbits(qv.Table):
+
+    orbit_id = qv.StringColumn(default=lambda: uuid.uuid4().hex)
+    object_id = qv.StringColumn(nullable=True)
+    weights = qv.Float64Column(nullable=True, validator=qv.and_(qv.ge(0), qv.le(1)))
+    weights_cov = qv.Float64Column(nullable=True, validator=qv.and_(qv.ge(0), qv.le(1)))
+    coordinates = CartesianCoordinates.as_column()
+
+    @classmethod
+    def create(
+        cls,
+        orbits: Orbits,
+        method: Literal["auto", "sigma-point", "monte-carlo"] = "auto",
+    ) -> "VariantOrbits":
+        """
+        Sample and create variants for the given orbits by sampling the covariance matrices.
+        There are three supported methods:
+        - sigma-point: Sample the covariance matrix using sigma points. This is the fastest method,
+        but can be inaccurate if the covariance matrix is not well behaved.
+        - monte-carlo: Sample the covariance matrix using a monte carlo method.
+        This is the slowest method, but is the most accurate. 10k samples are drawn.
+        - auto: Automatically select the best method based on the covariance matrix.
+        If the covariance matrix is well behaved then sigma-point sampling will be used.
+        If the covariance matrix is not well behaved then monte-carlo sampling will be used.
+
+        When sampling with monte-carlo, 10k samples are drawn. Sigma-point sampling draws 13 samples
+        for 6-dimensional coordinates.
+
+        Parameters
+        ----------
+        orbits : '~adam_core.orbits.orbits.Orbits'
+            The orbits for which to create variant orbits.
+        method : {'sigma-point', 'monte-carlo', 'auto'}, optional
+            The method to use for sampling the covariance matrix. If 'auto' is selected then the method
+            will be automatically selected based on the covariance matrix. The default is 'auto'.
+
+        Returns
+        -------
+        variants_orbits : '~adam_core.orbits.variants.VariantOrbits'
+            The variant orbits.
+        """
+        idx, W, W_cov, variant_coordinates = create_coordinate_variants(
+            orbits.coordinates, method=method
+        )
+        return cls.from_kwargs(
+            orbit_id=pc.take(orbits.orbit_id, idx),
+            object_id=pc.take(orbits.object_id, idx),
+            weights=W,
+            weights_cov=W_cov,
+            coordinates=variant_coordinates,
+        )
