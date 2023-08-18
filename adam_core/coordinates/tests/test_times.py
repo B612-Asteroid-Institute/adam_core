0cf41c84eca43969cc8efa59891a444449a9419f
Joachim Moeyens <moeyensj@gmail.com>
2023-08-18 05:56:44 -0700

    Add Times.from_mjd and Times.from_jd

--- adam_core/coordinates/tests/test_times.py
+++ adam_core/coordinates/tests/test_times.py
@@ -29,3 +29,30 @@ def test_Times_to_scale():
     times_utc = times_tdb.to_scale("utc")
     assert times_utc.scale == "utc"
     np.testing.assert_equal(times_utc.to_astropy().mjd, times_tdb.to_astropy().utc.mjd)
+
+
+def test_Times_from_jd():
+    # Test that we can create a Times object correctly from julian dates and a time scale
+    times_astropy = Time(
+        np.linspace(2450000.5, 2460000.5, 1000, dtype=np.double),
+        format="jd",
+        scale="tai",
+    )
+    times = Times.from_jd(times_astropy.jd, times_astropy.scale)
+
+    assert times.scale == times_astropy.scale
+    np.testing.assert_equal(times.jd().to_numpy(), times_astropy.jd)
+
+
+def test_Times_from_mjd():
+    # Test that we can create a Times object correctly from modified julian dates and a time scale
+    times_astropy = Time(
+        np.linspace(50000.0, 60000.0, 1000, dtype=np.double), format="mjd", scale="tai"
+    )
+    times = Times.from_mjd(times_astropy.mjd, times_astropy.scale)
+
+    # Surprised we can't do better here
+    assert times.scale == times_astropy.scale
+    np.testing.assert_allclose(
+        times.mjd().to_numpy(), times_astropy.mjd, rtol=0, atol=1e-9
+    )
