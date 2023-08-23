279c175eb2b1cc8e0c83188993cdcb4ca0a8d070
Joachim Moeyens <moeyensj@gmail.com>
2023-08-23 16:42:18 -0700

    Add unit test for rotations between ecliptic and equatorial coordinates

--- adam_core/coordinates/tests/conftest.py
+++ adam_core/coordinates/tests/conftest.py
@@ -16,9 +16,31 @@ def orbital_elements():
 
 
 @pytest.fixture
+def orbital_elements_equatorial():
+    orbital_elements_file = files("adam_core.utils.helpers.data").joinpath(
+        "elements_sun_eq.csv"
+    )
+    df = pd.read_csv(
+        orbital_elements_file, index_col=False, float_precision="round_trip"
+    )
+    return df
+
+
+@pytest.fixture
 def orbital_elements_barycentric():
     orbital_elements_file = files("adam_core.utils.helpers.data").joinpath(
         "elements_ssb_ec.csv"
+    )
+    df = pd.read_csv(
+        orbital_elements_file, index_col=False, float_precision="round_trip"
+    )
+    return df
+
+
+@pytest.fixture
+def orbital_elements_barycentric_equatorial():
+    orbital_elements_file = files("adam_core.utils.helpers.data").joinpath(
+        "elements_ssb_eq.csv"
     )
     df = pd.read_csv(
         orbital_elements_file, index_col=False, float_precision="round_trip"
