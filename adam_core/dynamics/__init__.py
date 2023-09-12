d5fac9e157f8188d42ac47993e16df121cc4adc3
Joachim Moeyens <moeyensj@gmail.com>
2023-09-12 15:55:27 -0700

    Fix propagate_2body, PointSourceDetections, and Exposures imports

--- adam_core/dynamics/__init__.py
+++ adam_core/dynamics/__init__.py
@@ -3,5 +3,6 @@ from .barker import solve_barker
 from .chi import calc_chi
 from .kepler import solve_kepler
 from .lagrange import apply_lagrange_coefficients, calc_lagrange_coefficients
+from .propagation import propagate_2body
 from .stumpff import calc_stumpff
 from .tisserand import calc_tisserand_parameter
---
+++ adam_core/observations/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa: F401
+from .detections import PointSourceDetections
+from .exposures import Exposures
