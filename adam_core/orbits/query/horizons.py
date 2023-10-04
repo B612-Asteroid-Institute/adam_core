c2ac69a04aa4e3a6471923a1b1586e56c76df3bb
Joachim Moeyens <moeyensj@users.noreply.github.com>
2023-10-03 22:07:52 -0700

    Set covariance matrices to nulls in horizons.py so they can be loaded from parquet (#74)

--- adam_core/orbits/query/horizons.py
+++ adam_core/orbits/query/horizons.py
@@ -2,11 +2,13 @@ from typing import List, Union
 
 import numpy.typing as npt
 import pandas as pd
+import pyarrow as pa
 from astropy.time import Time
 from astroquery.jplhorizons import Horizons
 
 from ...coordinates.cartesian import CartesianCoordinates
 from ...coordinates.cometary import CometaryCoordinates
+from ...coordinates.covariances import CoordinateCovariances
 from ...coordinates.keplerian import KeplerianCoordinates
 from ...coordinates.origin import Origin
 from ...coordinates.times import Times
@@ -281,6 +283,11 @@ def query_horizons(
             vx=vectors["vx"].values,
             vy=vectors["vy"].values,
             vz=vectors["vz"].values,
+            covariance=CoordinateCovariances.from_kwargs(
+                values=pa.FixedSizeListArray.from_arrays(
+                    pa.nulls(36 * len(vectors), pa.float64()), 36
+                )
+            ),
             origin=origin,
             frame=frame,
         )
@@ -312,6 +319,11 @@ def query_horizons(
             raan=elements["Omega"].values,
             ap=elements["w"].values,
             M=elements["M"].values,
+            covariance=CoordinateCovariances.from_kwargs(
+                values=pa.FixedSizeListArray.from_arrays(
+                    pa.nulls(36 * len(elements), pa.float64()), 36
+                )
+            ),
             origin=origin,
             frame=frame,
         )
@@ -346,6 +358,11 @@ def query_horizons(
             raan=elements["Omega"].values,
             ap=elements["w"].values,
             tp=tp.tdb.mjd,
+            covariance=CoordinateCovariances.from_kwargs(
+                values=pa.FixedSizeListArray.from_arrays(
+                    pa.nulls(36 * len(elements), pa.float64()), 36
+                )
+            ),
             origin=origin,
             frame=frame,
         )
