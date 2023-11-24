37ceca7acf8216d46ec1daea7116acec6350a901
Alec Koumjian <akoumjian@users.noreply.github.com>
2023-11-24 10:39:18 -0500

    Changes to using ray (#97)

---
+++ adam_core/ray_cluster.py
@@ -0,0 +1,41 @@
+import logging
+from typing import Optional
+
+import ray
+
+logger = logging.getLogger(__name__)
+
+
+def initialize_use_ray(
+    num_cpus: Optional[int] = None, object_store_bytes: Optional[int] = None
+) -> bool:
+    """
+    Ensures we use existing local cluster, or starts new one with desired resources
+    """
+    use_ray = False
+    if num_cpus is None or num_cpus > 1:
+        # Initialize ray
+        if not ray.is_initialized():
+            logger.info("Ray is not initialized. Initializing...")
+            # For some reason, ray does not seem to automatically
+            # find existing local clusters without `address="auto"`
+            # but it will fail if we use auto and there is no existing cluster.
+            # So we wrap it in a try/except, using an existing cluster if we can
+            # Otherwise starting fresh.
+            try:
+                logger.info("Attempting to connect to existing ray cluster...")
+                ray.init(address="auto")
+            except ConnectionError:
+                logger.info("Could not connect to existing ray cluster.")
+                logger.info(
+                    f"Attempting ray with {num_cpus} cpus and {object_store_bytes} bytes."
+                )
+                ray.init(
+                    num_cpus=num_cpus,
+                    object_store_memory=object_store_bytes,
+                )
+
+        logger.info(f"Ray Resources: {ray.cluster_resources()}")
+
+        use_ray = True
+    return use_ray
---
+++ adam_core/tests/test_ray_cluster.py
@@ -0,0 +1,37 @@
+from adam_core.ray_cluster import initialize_use_ray
+
+
+# Test that we are calling the correct ray init function using mocks
+def test_initialize_ray_no_cluster(mocker):
+    mock_ray = mocker.patch("adam_core.ray_cluster.ray")
+
+    # Ensure we initialize ray with the correct values
+    mock_ray.is_initialized.return_value = False
+    # Set mock to throw an error if init is called with address="auto"
+    # but not if called with other args
+    mock_ray.init.side_effect = [
+        ConnectionError,
+        None,
+    ]
+
+    initialize_use_ray(num_cpus=4, object_store_bytes=1000)
+    mock_ray.init.assert_called_with(num_cpus=4, object_store_memory=1000)
+    mock_ray.init.call_count == 1
+
+
+def test_initialize_ray_existing_cluster(mocker):
+    mock_ray = mocker.patch("adam_core.ray_cluster.ray")
+
+    mock_ray.is_initialized.return_value = False
+    mock_ray.init.return_value = True
+    initialize_use_ray()
+
+    mock_ray.init.assert_called_once_with(address="auto")
+    assert mock_ray.init.call_count == 1
+
+
+def test_initialize_ray_fully_initialized(mocker):
+    mock_ray = mocker.patch("adam_core.ray_cluster.ray")
+    mock_ray.is_initialized.return_value = True
+    initialize_use_ray()
+    assert mock_ray.init.call_count == 0
