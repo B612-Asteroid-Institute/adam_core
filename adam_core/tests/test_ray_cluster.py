5bec52689b72abc9c62a08319caae87ac0151f04
Alec Koumjian <akoumjian@gmail.com>
2023-11-24 10:27:56 -0500

    mocker does not register call if exception is side effect

--- adam_core/tests/test_ray_cluster.py
+++ adam_core/tests/test_ray_cluster.py
@@ -2,7 +2,7 @@ from adam_core.ray_cluster import initialize_use_ray
 
 
 # Test that we are calling the correct ray init function using mocks
-def test_initialize_ray(mocker):
+def test_initialize_ray_no_cluster(mocker):
     mock_ray = mocker.patch("adam_core.ray_cluster.ray")
 
     # Ensure we initialize ray with the correct values
@@ -15,11 +15,13 @@ def test_initialize_ray(mocker):
     ]
 
     initialize_use_ray(num_cpus=4, object_store_bytes=1000)
-    mock_ray.init.assert_called_with(address="auto")
     mock_ray.init.assert_called_with(num_cpus=4, object_store_memory=1000)
-    mock_ray.init.call_count == 2
+    mock_ray.init.call_count == 1
 
-    mock_ray.reset_mock(return_value=True, side_effect=True)
+
+def test_initialize_ray_existing_cluster(mocker):
+    mock_ray = mocker.patch("adam_core.ray_cluster.ray")
+
     mock_ray.is_initialized.return_value = False
     mock_ray.init.return_value = True
     initialize_use_ray()
@@ -27,7 +29,9 @@ def test_initialize_ray(mocker):
     mock_ray.init.assert_called_once_with(address="auto")
     assert mock_ray.init.call_count == 1
 
-    mock_ray.reset_mock(return_value=True, side_effect=True)
+
+def test_initialize_ray_fully_initialized(mocker):
+    mock_ray = mocker.patch("adam_core.ray_cluster.ray")
     mock_ray.is_initialized.return_value = True
     initialize_use_ray()
     assert mock_ray.init.call_count == 0
