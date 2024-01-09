ad87a07c880e3caff646f58cd9cc728037fb544b
Alec Koumjian <akoumjian@users.noreply.github.com>
2024-01-09 10:11:18 -0500

    Add utility to return indices of chunks (#103)

--- adam_core/propagator/utils.py
+++ adam_core/propagator/utils.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Sequence
+from typing import Iterable, Sequence, Tuple
 
 import numpy as np
 
@@ -25,6 +25,30 @@ def _iterate_chunks(iterable: Sequence, chunk_size: int) -> Iterable:
     N = len(iterable)
     for i in range(0, N, chunk_size):
         yield iterable[i : i + chunk_size]
+
+
+def _iterate_chunk_indices(
+    iterable: Sequence, chunk_size: int
+) -> Iterable[Tuple[int, int]]:
+    """
+    Generator that yields indices for chunks of size chunk_size from sized
+    iterable (Sequence).
+
+    Parameters
+    ----------
+    iterable : Sequence
+        Iterable to chunk.
+    chunk_size : int
+        Size of chunks.
+
+    Yields
+    ------
+    chunk : iterable
+        Chunk of size chunk_size from iterable.
+    """
+    N = len(iterable)
+    for i in range(0, N, chunk_size):
+        yield i, min(i + chunk_size, N)
 
 
 def _assert_times_almost_equal(
