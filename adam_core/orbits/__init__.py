bb29920bc07b84d15b864f4c86ac6dadcd396859
Kathleen Kiker <kathleen@b612foundation.orgm>
2024-04-24 12:59:10 -0700

    Initial WIP of ip module

--- adam_core/orbits/__init__.py
+++ adam_core/orbits/__init__.py
@@ -1,13 +1,15 @@
 # flake8: noqa: F401
 
 # TODO: calc_orbit_class does not work currently, but would be nice as a method on Orbits
-from .classification import calc_orbit_class
+from .impacts import calculate_impacts
+from .impacts import calculate_impact_probabilities
 from .ephemeris import Ephemeris
 from .orbits import Orbits
 from .variants import VariantOrbits
 
 __all__ = [
-    "calc_orbit_class",
+    "calculate_impacts",
+    "calculate_impact_probabilities",
     "Ephemeris",
     "Orbits",
     "VariantOrbits",
---
+++ adam_core/orbits/impacts.py
@@ -0,0 +1,103 @@
+from .variants import VariantOrbits
+import quivr as qv
+from typing import Optional
+from adam_core.propagator.propagator import Propagator
+
+import numpy as np
+from astropy.time import Time
+from astropy.coordinates import get_body_barycentric, solar_system_ephemeris
+from astropy.constants import au
+import astropy.units as u
+import math
+from collections import Counter
+from adam_core.coordinates import CartesianCoordinates
+
+class EarthImpacts(qv.Table):
+    orbit_id = qv.StringColumn()
+    # Distance from earth center in km
+    distance = qv.Float64Column()
+    coordinates = CartesianCoordinates.as_column()
+    variant_id = qv.StringColumn()
+
+
+def calculate_impacts(orbits, num_days, propagator, num_samples: int = 1000):
+    """
+    Calculate the impacts for each variant orbit generated from the input orbits.
+
+    Parameters
+    ----------
+    orbits : `~adam_core.orbits.orbits.Orbits`
+        Orbits for which to calculate impact probabilities.
+    num_days : int
+        Number of days to propagate the orbits.
+    propagator : `~adam_core.propagator.propagator.Propagator`
+        Propagator to use for orbit propagation.
+    num_samples : int, optional
+        Number of samples to take over the period, by default 1000.
+
+    Returns
+    -------
+
+    """
+
+    earth_impacts = None
+    variants = VariantOrbits.create(
+                                orbits, method='monte-carlo', num_samples=num_samples, 
+                            )
+    for i, var_orbit in enumerate(variants):
+        variant_id = f"{var_orbit.orbit_id[0]}_var{i}"
+        var_orbit.variant_id = variant_id
+        results, impact = propagator._propagate_orbits_inner(var_orbit, var_orbit.coordinates.time.add_days(num_days)[0], detect_impacts=True, adaptive_mode=2, min_dt=1e-15)   
+        if impact is not None:
+            if earth_impacts is None:
+                earth_impacts = EarthImpacts.from_kwargs(
+                    orbit_id=impact.orbit_id,
+                    distance=impact.distance,
+                    coordinates=impact.coordinates,
+                    variant_id=variant_id,
+                )
+            else:
+                earth_impacts = qv.concatenate(
+                    earth_impacts,
+                    EarthImpacts.from_kwargs(
+                        orbit_id=impact.orbit_id,
+                        distance=impact.distance,
+                        coordinates=impact.coordinates,
+                        variant_id=variant_id,
+                    ),
+                )
+
+    return variants, earth_impacts
+
+
+def calculate_impact_probabilities(variants, impacts):
+    """
+    Calculate the impact probabilities for each variant orbit generated from the input orbits.
+
+    Parameters
+    ----------
+    variants : `~adam_core.orbits.variants.VariantOrbits`
+        Variant orbits for which to calculate impact probabilities.
+    impacts : `~adam_core.orbits.impacts.Impacts`
+        Impacts for the variant orbits.
+
+    Returns
+    -------
+    impact_probabilities : `~adam_core.orbits.impact_probabilities.ImpactProbabilities`
+        Impact probabilities for the variant orbits.
+    """
+
+    # Count the number of impacts for each variant orbit.
+    impact_counts = Counter(impacts.variant_id)
+
+    # Calculate the impact probabilities for each variant orbit.
+    impact_probabilities = qv.Table(
+        variant_id=variants.variant_id,
+        impact_probability=[
+            impact_counts.get(variant_id, 0) / len(variants)
+            for variant_id in variants.variant_id
+        ],
+    )
+
+    return impact_probabilities
+
