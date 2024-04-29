e5d45e13defe012f08ae5f80ab7cf9e595d0378d
Kathleen Kiker <kathleen@b612foundation.orgm>
2024-04-29 10:33:56 -0700

    Fixes

--- adam_core/orbits/impacts.py
+++ adam_core/orbits/impacts.py
@@ -1,18 +1,14 @@
-from .variants import VariantOrbits
-import quivr as qv
-from typing import Optional
-from adam_core.propagator.propagator import Propagator
-import pyarrow as pa
-import pyarrow.compute as pc
-
-import numpy as np
-from astropy.time import Time
-from astropy.coordinates import get_body_barycentric, solar_system_ephemeris
-from astropy.constants import au
-import astropy.units as u
 import math
-from collections import Counter
+
+import pyarrow.compute as pc
+import quivr as qv
+
 from adam_core.coordinates import CartesianCoordinates
+from adam_core.coordinates.chi2 import calculate_chi2
+from adam_core.coordinates.residuals import Residuals
+
+from .variants import VariantOrbits
+
 
 class EarthImpacts(qv.Table):
     orbit_id = qv.LargeStringColumn()
@@ -44,10 +40,18 @@ def calculate_impacts(orbits, num_days, propagator, num_samples: int = 1000):
 
     impact_list = []
     variants = VariantOrbits.create(
-                                orbits, method='monte-carlo', num_samples=num_samples, 
-                            )
+        orbits,
+        method="monte-carlo",
+        num_samples=num_samples,
+    )
     for i, var_orbit in enumerate(variants):
-        results, impact = propagator._propagate_orbits_inner(var_orbit, var_orbit.coordinates.time.add_days(num_days)[0], detect_impacts=True, adaptive_mode=2, min_dt=1e-15)   
+        results, impact = propagator._propagate_orbits_inner(
+            var_orbit,
+            var_orbit.coordinates.time.add_days(num_days)[0],
+            detect_impacts=True,
+            adaptive_mode=2,
+            min_dt=1e-15,
+        )
         if impact is not None:
             earth_impact = EarthImpacts.from_kwargs(
                 variant_id=[var_orbit.variant_id[0]],
@@ -79,7 +83,7 @@ def calculate_impact_probabilities(variants, impacts):
         Impact probabilities for the variant orbits.
     """
 
-    #Loop through the unique set of orbit_ids within variants using quivr
+    # Loop through the unique set of orbit_ids within variants using quivr
     unique_orbits = pc.unique(variants.orbit_id)
     print(unique_orbits)
 
@@ -92,7 +96,7 @@ def calculate_impact_probabilities(variants, impacts):
         impacts_mask = pc.equal(impacts.orbit_id, orbit_id)
         impacts_masked = impacts.table.filter(impacts_mask)
         impact_count = len(impacts_masked)
-        ip = impact_count/variant_count
+        ip = impact_count / variant_count
 
         ip_dict[orbit_id] = ip
         print(ip_dict)
@@ -128,3 +132,30 @@ def return_impacting_variants(variants, impacts):
         },
     )
 
+
+def calculate_mahalanobis_distance(
+    observed_orbit, observed_covariance, predicted_orbit
+):
+    """
+    Calculate the Mahalanobis distance between an observed orbit and a predicted orbit.
+
+    Parameters
+    ----------
+    observed_orbit : `~adam_core.coordinates.CartesianCoordinates`
+        Observed orbit.
+    observed_covariance : `~adam_core.coordinates.CartesianCoordinates`
+        Covariance of the observed orbit.
+    predicted_orbit : `~adam_core.coordinates.CartesianCoordinates`
+        Predicted orbit.
+
+    Returns
+    -------
+    mahalanobis_distance : float
+        Mahalanobis distance between the observed and predicted orbits.
+    """
+
+    residuals = Residuals.calculate_residuals(observed_orbit, predicted_orbit)
+    mahalanobis_distance2 = calculate_chi2(residuals, observed_covariance)
+    mahalanobis_distance = math.sqrt(mahalanobis_distance2)
+
+    return mahalanobis_distance
