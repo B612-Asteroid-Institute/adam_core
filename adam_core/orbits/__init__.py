9a249c1c60de3812f07ab7d280b163c4115ef1c5
Kathleen Kiker <kathleen@b612foundation.orgm>
2024-04-30 21:43:50 -0700

    Precommit changes

--- adam_core/orbits/__init__.py
+++ adam_core/orbits/__init__.py
@@ -1,17 +1,15 @@
 # flake8: noqa: F401
 
 # TODO: calc_orbit_class does not work currently, but would be nice as a method on Orbits
-from .impacts import calculate_impacts
-from .impacts import calculate_impact_probabilities
 from .ephemeris import Ephemeris
+from .impacts import calculate_impact_probabilities, calculate_impacts
 from .orbits import Orbits
 from .variants import VariantOrbits
 
 __all__ = [
     "calculate_impacts",
     "calculate_impact_probabilities",
-    "return_impacting_variants"
-    "Ephemeris",
+    "return_impacting_variants" "Ephemeris",
     "Orbits",
     "VariantOrbits",
 ]
