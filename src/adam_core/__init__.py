5eab8654be4d0259cefda818c96c9a2e56702a22
Alec Koumjian <akoumjian@gmail.com>
2024-05-24 10:23:52 -0400

    Go back to scm based tag versioning

--- src/adam_core/__init__.py
+++ src/adam_core/__init__.py
@@ -1,5 +1,4 @@
-from .__version__ import __version__
-
-__all__ = [
-    "__version__",
-]
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0dev0"
